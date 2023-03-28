e01da2a99a4ea0b192564556519bdc1b6761a9c9
bzwyant <bzwyant@gmail.com>
2023-03-27 21:35:23 -0400

    normalize data method added

--- src/neo4j_driver.py
+++ src/neo4j_driver.py
@@ -32,6 +32,14 @@ class Neo4jDriver():
         self.sim_scores: np.ndarray = np.empty((0,3))
         self.random_nodes:list = []
         self.artists_nodes: list = []
+        
+        self.track_keys: list[str] = ['id', 'artist', 'album', 'name', 'popularity', 'duration_ms', 'explicit',
+                                      'danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness',
+                                      'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo',
+                                      'time_signature', 'genre']
+
+        # Define a list of keys to exclude from the array
+        self.exclude_keys: list[str] = ['artist', 'album', 'name', 'genre', 'id']
 
     def connect(self) -> None:
         """
@@ -182,15 +190,13 @@ class Neo4jDriver():
         """
         Calculates the similarity score between two tracks based on their attribute values.
         """
-
+        
         def process_dict(d) -> np.ndarray:
-            # Define a list of keys to exclude from the array
-            exclude_keys: list[str] = ['artist', 'album', 'name', 'genre', 'id']
             # MATCH (p:Track) RETURN MAX(p.ATTRIBUTE YOU WANT) AS max_price, MIN(p.ATTRIBUTE YOU WANT) AS min_price
             # Create a list of numerical values, mapping True/False to 1/0
             values:list = []
             for key, value in d.items():
-                if key not in exclude_keys:
+                if key not in self.exclude_keys:
                     if isinstance(value, bool):
                         values.append(int(value))
                     elif isinstance(value, (int, float)):
@@ -226,6 +232,27 @@ class Neo4jDriver():
                     if similarity_score > threshold:
                         self.create_relationship(node_artist, pair_node, "MATCHED", f"{{sim_score: {similarity_score}}}")
 
+    def normalize_data(self) -> np.ndarray:
+        # Get the top recommended songs
+        with self.driver.session() as session:
+            for key in self.track_keys:
+                if key not in self.exclude_keys:
+                    # normalize all the features
+                    query: str = f"MATCH (t:Track) RETURN MAX(t.{key}) AS max_{key}, MIN(t.{key}) AS min_{key}"
+                    result = session.run(query)
+                    record = result.single()
+                    self.max_min_values[key] = (record[f"max_{key}"], record[f"min_{key}"])
+            # normalize all features for all nodes
+            all_tracks = session.run("MATCH (t:Track) RETURN t")
+            for track in all_tracks:
+                for key in self.track_keys:
+                    if key not in self.exclude_keys:
+                        # Update the track feature in the database
+                        track_feature = track['t']._properties[key]
+                        normalized_feature = (track_feature - self.max_min_values[key][1]) / (
+                                    self.max_min_values[key][0] - self.max_min_values[key][1])
+                        query = f"MATCH (t:Track) WHERE ID(t) = {track['t']._id} SET t.{key} = {normalized_feature}"
+                        session.run(query)
     def random_sample(self, batch_size=1500, artist="Regina Spektor") -> None:
         with self.driver.session() as session:
             query:str = f"MATCH (t:Track) WHERE NOT t.artist = '{artist}' WITH t, rand() AS r ORDER BY r RETURN ID(t) AS track_id LIMIT {batch_size}"
@@ -243,7 +270,7 @@ class Neo4jDriver():
         """
         Given a track ID, finds recommended songs using the specified similarity metric and threshold.
         """
-        recommended_songs:list = []
+        recommended_songs: list = []
 
         # Create the relationships across the entire graph 
         self.evaluate_metrics()
